79882f74e0f29b14469876c4c25e88d818adc886
Abhijeet Shah <ashah@albany.edu>
2022-02-15 16:28:23 +0530

    Initial Commit

---
+++ Transitions/TransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransitionsApp.swift
+//  Transitions
+//
+//  Created by Abhijeet Shah on 2/15/22.
+//
+
+import SwiftUI
+
+@main
+struct TransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
